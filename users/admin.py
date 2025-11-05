60afcd6018aabdb10f7aa266184629b4da409a69
Donghyeok Im <fradhyle@gmail.com>
2025-11-05 14:55:07 +0900

    New start with Django 5.2.7
    - Changed Django version to 5.2.7.
    - Removed multilingual support.
    - New user model.

--- users/admin.py
+++ users/admin.py
@@ -1,18 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-
-from users.models import User
-
 
 # Register your models here.
-@admin.register(User)
-class CustomUserAdmin(UserAdmin):
-    list_display = (
-        "phone_number",
-        "surname",
-        "given_name",
-        "birthday",
-    )
-    ordering = ("phone_number",)
-    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("phone_number",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("phone_number",)}),)
--- users/models.py
+++ users/models.py
@@ -1,4 +1,3 @@
-import uuid
 from typing import Final
 
 from django.contrib.auth.models import (
@@ -7,10 +6,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.db import models
-from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
 
-from branches.models import Branch
 from IMX.validators import phone_number_validator
 
 
@@ -21,39 +17,20 @@ class UserManager(BaseUserManager):
         phone_number,
         surname,
         given_name,
-        birthday,
+        birth_date,
         gender,
         password=None,
+        **kwargs,
     ):
         user = self.model(
             phone_number=phone_number,
             surname=surname,
             given_name=given_name,
-            birthday=birthday,
+            birth_date=birth_date,
             gender=gender,
+            **kwargs,
         )
         user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_staff(
-        self,
-        phone_number,
-        surname,
-        given_name,
-        birthday,
-        gender,
-        password=None,
-    ):
-        user = self.create_user(
-            phone_number=phone_number,
-            surname=surname,
-            given_name=given_name,
-            birthday=birthday,
-            gender=gender,
-            password=password,
-        )
-        user.is_staff = True
         user.save(using=self._db)
         return user
 
@@ -62,170 +39,80 @@ class UserManager(BaseUserManager):
         phone_number,
         surname,
         given_name,
-        birthday,
+        birth_date,
         gender,
         password=None,
+        **kwargs,
     ):
-        user = self.create_user(
-            surname=surname,
-            phone_number=phone_number,
-            given_name=given_name,
-            birthday=birthday,
-            gender=gender,
-            password=password,
+        kwargs.setdefault("is_staff", True)
+        kwargs.setdefault("is_superuser", True)
+        return self.create_user(
+            phone_number, surname, given_name, birth_date, gender, password, **kwargs
         )
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    GENDERS: Final[dict] = {
-        1: _("남성"),
-        2: _("여성"),
-    }
-    serial = models.BigAutoField(
-        verbose_name=_("연번"),
-        primary_key=True,
-    )
+    class Gender(models.IntegerChoices):
+        MALE = 1, "남성"
+        FEMALE = 2, "여성"
+
     phone_number = models.CharField(
-        verbose_name=_("전화번호"),
-        max_length=14,
+        max_length=15,
+        unique=True,
         validators=[
             phone_number_validator,
         ],
-        unique=True,
+        verbose_name="전화번호",
     )
     surname = models.CharField(
-        verbose_name=_("성"),
-        max_length=20,
+        max_length=30,
+        verbose_name="성",
     )
     given_name = models.CharField(
-        verbose_name=_("이름"),
-        max_length=20,
+        max_length=30,
+        verbose_name="이름",
     )
-    email = models.EmailField(
-        verbose_name=_("이메일 주소"),
-        blank=True,
-        null=True,
+    birth_date = models.DateField(
+        verbose_name="생년월일",
     )
-    password = models.TextField(
-        verbose_name=_("암호"),
-    )
-    birthday = models.DateField(
-        verbose_name=_("생일"),
-    )
-    gender = models.PositiveIntegerField(
-        verbose_name=_("성별"),
-        choices=GENDERS,
+    gender = models.PositiveSmallIntegerField(
+        choices=Gender.choices,
+        verbose_name="성별",
     )
     is_active = models.BooleanField(
-        verbose_name=_("활성화 여부"),
         default=True,
+        verbose_name="활성 상태",
     )
     is_staff = models.BooleanField(
-        verbose_name=_("직원 여부"),
         default=False,
+        verbose_name="관리자 권한",
     )
     is_superuser = models.BooleanField(
-        verbose_name=_("슈퍼유저 여부"),
         default=False,
-    )
-    last_login = models.DateTimeField(
-        verbose_name=_("최근 로그인"),
-        default=timezone.now,
+        verbose_name="최고 관리자 권한",
     )
     date_joined = models.DateTimeField(
-        verbose_name=_("가입일"),
-        default=timezone.now,
+        auto_now_add=True,
+        verbose_name="가입일",
     )
 
     objects = UserManager()
 
-    @property
-    def full_name(self):
-        return f"{self.surname} {self.given_name}"
-
-    def __str__(self):
-        return self.full_name
-
-    class Meta:
-        verbose_name = _("이용자")
-        verbose_name_plural = _("이용자")
-
-    USERNAME_FIELD = "phone_number"
-    EMAIL_FIELD = "email"
-    REQUIRED_FIELDS = [
+    USERNAME_FIELD: Final[str] = "phone_number"
+    REQUIRED_FIELDS: list[str] = [
         "surname",
         "given_name",
-        "birthday",
+        "birth_date",
         "gender",
     ]
 
-
-class UserBranch(models.Model):
-    name = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-    )
-    branch = models.ForeignKey(
-        Branch,
-        to_field="serial",
-        on_delete=models.DO_NOTHING,
-    )
-
     class Meta:
-        verbose_name = _("이용자 지점")
+        verbose_name: str = "사용자"
+        verbose_name_plural: str = "사용자"
 
+    @property
+    def full_name(self) -> str:
+        return f"{self.surname} {self.given_name}"
 
-class UserLicenseType(models.Model):
-    LICENSE_TYPES: Final[dict] = {
-        "1L": _("1종 대형"),
-        "1O": _("1종 보통"),
-        "1OA": _("1종 보통 (자동)"),
-        "2O": _("2종 보통"),
-        "2OA": _("2종 보통 (자동)"),
-        "IL": _("장롱 면허"),
-    }
-
-    username = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-    )
-    license_type = models.CharField(
-        max_length=3,
-        verbose_name=_("면허 유형"),
-        choices=LICENSE_TYPES,
-        null=True,
-        default=None,
-    )
-
-    class Meta:
-        verbose_name = _("이용자 면허 유형")
-
-
-class UserPlanType(models.Model):
-    PLAN_TYPES: Final[dict] = {
-        "T": _("시간제"),
-        "GA": _("합격 보장제"),
-        "GC": _("코스 보장제"),
-        "GR": _("도로 보장제"),
-        "LD": _("장롱 면허"),
-    }
-
-    username = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-    )
-    plan_type = models.CharField(
-        max_length=2,
-        verbose_name=_("요금제 유형"),
-        choices=PLAN_TYPES,
-        blank=True,
-        null=True,
-        default=None,
-    )
-
-    class Meta:
-        verbose_name = _("이용자 요금제")
+    def __str__(self) -> str:
+        return self.full_name
